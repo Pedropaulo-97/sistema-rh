c8cf92c17ccc02009d783ec353fc18d0419f6fda
Pedropaulo-97 <pedropaulo.np97@gmail.com>
2026-05-20 23:03:29 -0300

    feat(main): adiciona funcionaria maria oliveira e calculo de bonus

--- src/main/java/br/com/rh/Main.java
+++ src/main/java/br/com/rh/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Funcionario joao = new Funcionario(
-          "João Silva",
+                "João Silva",
                 "123.456.789-00",
                 5000.0,
                 2,
@@ -16,9 +16,23 @@ public class Main {
                 "Tecnologia"
         );
 
-        double bonus = joao.calcularBonus(3, 8.5);
+        Funcionario maria = new Funcionario(
+                "Maria Oliveira",
+                "987.654.321-00",
+                8000.0,
+                1,
+                true,
+                "15/06/2019",
+                "Financeiro"
+        );
+
+        double bonus1 = joao.calcularBonus(3, 8.5);
+        double bonus2 = maria.calcularBonus(5, 6.5);
 
         System.out.println(joao);
-        System.out.println("Bônus anual: R$ " + bonus);
+        System.out.println("Bônus anual: R$ " + bonus1);
+
+        System.out.println(maria);
+        System.out.println("Bônus anual: R$ " + bonus2);
     }
 }
